9063ef6d934e9e9241882a5b27328830fbba9c87
Kornull <wolod1987kor@gmail.com>
2022-01-28 17:20:50 +0300

    feat: active button color

--- portfolio/assets/js/replaceImg.js
+++ portfolio/assets/js/replaceImg.js
@@ -1,26 +1,37 @@
 const portfolioImage = document.querySelectorAll('.work__img');
-let btnn = document.querySelector('.btn__portfolio')
+const portfolioBtn = document.querySelector('.btn__portfolio')
+const portfolioBtns = document.querySelector('.porfolio__buttons');
+
 // Change images
 function changeImage() {
-  const portfolioBtns = document.querySelector('.porfolio__buttons');
   portfolioBtns.addEventListener('click', (event) => {
     if (event.target.classList.contains('btn__portfolio')) {
       let seasons = event.target.dataset.season;
-      portfolioImage.forEach((el, i) => el.src = `./assets/img/imagesPortfolio/${seasons}/${i + 1}.jpg`
-      )
+      portfolioImage.forEach((el, i) => el.src = `./assets/img/imagesPortfolio/${seasons}/${i + 1}.jpg`);
     }
-  })
+  });
 }
-changeImage()
+changeImage();
 
 // Cache images
 const allSeasons = ['winter', 'spring', 'summer', 'autumn'];
 function preloadSummerImages() {
   const img = new Image();
-  allSeasons.forEach(el =>{
-  for(let i = 1; i <= 6; i++) {
-    img.src = `./assets/img/imagesPortfolio/${el}/${i}.jpg`;}
-  // console.log(img.src)}
-  })
+  allSeasons.forEach(el => {
+    for (let i = 1; i <= 6; i++) {
+      img.src = `./assets/img/imagesPortfolio/${el}/${i}.jpg`;
+    }
+  });
 }
 preloadSummerImages();
+
+// Color button
+const allBtnsPortfolio = document.querySelectorAll('.btn__portfolio')
+
+function changeClassActive() {
+  portfolioBtns.addEventListener('click', (event) => {
+    allBtnsPortfolio.forEach(cl => cl.classList.remove('active'));
+    event.target.classList.add('active');
+  });
+}
+changeClassActive();
