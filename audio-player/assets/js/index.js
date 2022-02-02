ae80bb282f6467efd00028671ce2203d155a71ba
Kornull <wolod1987kor@gmail.com>
2022-02-02 21:28:48 +0300

    fix: push for test/fixed links app

--- audio-player/assets/js/index.js
+++ audio-player/assets/js/index.js
@@ -3,55 +3,60 @@ const btnPrev = document.querySelector('.btn__left');
 const btnNext = document.querySelector('.btn__right');
 const imgMusic = document.querySelector('.atrist__image')
 
-let numtrack = 0
+let numTrack = 0
 const music = [
-  '/audio-player/assets/audio/Bring Me The Horizon - Kingslayer.mp3',
-  '/audio-player/assets/audio/Children of Bodom - Oops! I Did It Again.mp3',
-   '/audio-player/assets/audio/Malia J - Smells Like Teen Spirit.mp3',
+  '../assets/audio/Bring Me The Horizon - Kingslayer.mp3',
+  '../assets/audio/Children of Bodom - Oops! I Did It Again.mp3',
+   '../audio/Malia J - Smells Like Teen Spirit.mp3',
 ]
 let i = 1;
 const audio = new Audio();
-function playAudio(num) {
-  audio.src = music[numtrack];
-  audio.currentTime = 0;
-  audio.play();
-}
-function pauseAudio() {
-  audio.pause();
-}
+
+
+
+
 btnPlay.addEventListener('click', (ev) => {
   if (i % 2 !== 0) {
     btnPlay.classList.add('play');
 
     playAudio();
     i++;
-    // console.log(i);
   }
   else if (i % 2 === 0) {
     btnPlay.classList.remove('play');
     pauseAudio();
     i = 1
-    // console.log(i);
   }
 })
 btnNext.addEventListener('click', (ev) => {
-  numtrack++;
-  if (numtrack > music.length - 1) {
-    numtrack = 0;
+  numTrack++;
+  if (numTrack > music.length - 1) {
+    numTrack = 0;
   }
   i+=2;
   console.log('i++',i)
-  playAudio(music[numtrack]);
+  playAudio(music[numTrack]);
 })
 
 btnPrev.addEventListener('click', (ev) => {
-  numtrack--;
-  if (numtrack < 0) {
-    numtrack = music.length-1;
+  numTrack--;
+  if (numTrack < 0) {
+    numTrack = music.length-1;
   }
-
   i-=2;
   if (i<0){i = 2}
   console.log('i--',i)
-  playAudio(music[numtrack]);
+  playAudio(music[numTrack]);
 })
+
+
+
+function playAudio() {
+  audio.src = music[numTrack];
+  audio.currentTime = 0;
+  audio.play();
+  imgMusic.src=`../assets/img/imgMusic/${numTrack}.jpg`
+}
+function pauseAudio() {
+  audio.pause();
+}
