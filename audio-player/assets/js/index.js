8dfbd572e9c2464ebde2879f7cb78b12a740121c
Kornull <wolod1987kor@gmail.com>
2022-02-02 21:01:50 +0300

    feat: add prew/next btns

--- audio-player/assets/js/index.js
+++ audio-player/assets/js/index.js
@@ -1,26 +1,56 @@
-const audio = document.querySelector('audio');
-const btnPlay = document.querySelector('.btn__play')
+const btnPlay = document.querySelector('.btn__play');
+const btnPrev = document.querySelector('.btn__left');
+const btnNext = document.querySelector('.btn__right');
+const imgMusic = document.querySelector('.atrist__image')
 
+let numtrack = 0
+const music = [
+  '/audio-player/assets/audio/Bring Me The Horizon - Kingslayer.mp3',
+  '/audio-player/assets/audio/Children of Bodom - Oops! I Did It Again.mp3',
+   '/audio-player/assets/audio/Malia J - Smells Like Teen Spirit.mp3'
+]
 let i = 1;
-function playAudio() {
+const audio = new Audio();
+function playAudio(num) {
+  audio.src = music[numtrack];
   audio.currentTime = 0;
   audio.play();
 }
 function pauseAudio() {
   audio.pause();
 }
-
 btnPlay.addEventListener('click', (ev) => {
   if (i % 2 !== 0) {
-    btnPlay.classList.add('play')
-    playAudio()
-    i++
-    console.log(i)
+    btnPlay.classList.add('play');
+
+    playAudio();
+    i++;
+    // console.log(i);
   }
   else if (i % 2 === 0) {
-    btnPlay.classList.remove('play')
-    pauseAudio()
-    i=1
-    console.log(i)
+    btnPlay.classList.remove('play');
+    pauseAudio();
+    i = 1
+    // console.log(i);
   }
+})
+btnNext.addEventListener('click', (ev) => {
+  numtrack++;
+  if (numtrack > music.length) {
+    numtrack = 0;
+  } btnPlay.classList.add('play');
+  i+=2;
+  console.log('i++',i)
+  playAudio(music[numtrack]);
+})
+
+btnPrev.addEventListener('click', (ev) => {
+  numtrack--;
+  if (numtrack < 0) {
+    numtrack = music.length - 1;
+  }
+  btnPlay.classList.add('play');
+  i-=2;
+  console.log('i--',i)
+  playAudio(music[numtrack]);
 })
