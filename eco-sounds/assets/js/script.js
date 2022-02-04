b10dae5fa6431aa0e8c74f65783f95a3e49a430a
Kornull <wolod1987kor@gmail.com>
2022-02-04 16:17:59 +0300

    feat: add all song

--- eco-sounds/assets/js/script.js
+++ eco-sounds/assets/js/script.js
@@ -1,22 +1,35 @@
 const btnPlay = document.querySelector('.btn__play--pause');
 const classes = document.querySelectorAll('[data-set]');
+const birdList = document.querySelector('.bird__list')
+const birdName = document.querySelectorAll('[data-set]')
 
 const birdSong = [
-  {'drozd':'drozd.mp3'},
-  {'javoronok':'javoronok.mp3'},
-  {'slavka':'slavka.mp3'},
-  {'solovey':'solovey.mp3'},
-  {'zarynka':'zarynka.mp3'},
-  {'forest':'forest.mp3'}
+  { 'drozd': './assets/audio/drozd.mp3' },
+  { 'javoronok': './assets/audio/javoronok.mp3' },
+  { 'slavka': './assets/audio/slavka.mp3' },
+  { 'solovey': './assets/audio/solovey.mp3' },
+  { 'zarynka': './assets/audio/zarynka.mp3' },
+  { 'forest': './assets/audio/forest.mp3' }
 ]
+// console.log(birdList)
+birdName.forEach(x => console.log(x))
+birdList.addEventListener('click', (ev) => {
+  let clickBird = ev.target.dataset.set
+  birdSong.forEach((el) => {
+    if (el[clickBird] !== undefined) {
+      isPlay = true
+      playAudio(el[clickBird]) }
+  })
+})
 
 let isPlay = false;
 const audio = new Audio()
 
-function playAudio() {
-  audio.src = "/assets/audio/forest.mp3"
+function playAudio(clickBird) {
+  audio.src = clickBird
   audio.currentTime = 0;
   if (isPlay === true) {
+    btnPlay.classList.add('play');
     audio.play();
   }
   else {
@@ -24,6 +37,7 @@ function playAudio() {
   }
 }
 function pauseAudio() {
+  btnPlay.classList.remove('play');
   audio.pause()
 }
 
@@ -32,12 +46,10 @@ btnPlay.addEventListener('click', (ev) => {
   console.log('aaaa')
   if (isPlay === false) {
     isPlay = true;
-    btnPlay.classList.add('play');
     playAudio();
   }
   else if (isPlay === true) {
     isPlay = false;
-    btnPlay.classList.remove('play');
     pauseAudio();
   }
 })
