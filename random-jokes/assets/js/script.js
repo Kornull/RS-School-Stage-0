d2f0d475179bf2871b35b78ac0c97985db194709
Kornull <wolod1987kor@gmail.com>
2022-02-10 10:58:42 +0300

    feat: add random download

--- random-jokes/assets/js/script.js
+++ random-jokes/assets/js/script.js
@@ -1,1 +1,19 @@
-url = ''
+const text = document.querySelector('.text')
+const author = document.querySelector('.author')
+
+// const url = 'https://type.fit/api/quotes'
+async function getQuotes() {
+  const quotes = '/assets/json/belarusian_quotes.json';
+  const res = await fetch(quotes);
+  const data = await res.json();
+  min = Math.ceil(0);
+  max = Math.floor(data.length);
+  let aut = data[Math.floor(Math.random() * (max - min)) + min];
+  author.textContent = `${aut['author']}`
+  text.textContent = `${aut['text']}`
+}
+getQuotes();
+
+
+
+// getData();
