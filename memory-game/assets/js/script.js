54394cd42f65234b8037ada6a699a6f82d479e2f
Kornull <wolod1987kor@gmail.com>
2022-02-17 20:19:11 +0300

    fear: add shuffle card

--- memory-game/assets/js/script.js
+++ memory-game/assets/js/script.js
@@ -1,11 +1,15 @@
 const cards = document.querySelectorAll('.card')
-
+document.querySelector('.reset').addEventListener('click', resetCard)
 let hasFlipCard = false;
+let blockCard = false;
+let cardlist = [];
 let oneCard, twoCard;
 
 // track click
-cards.forEach((x) => {
+cards.forEach((x, i) => {
+  cardlist.push(i)
   x.addEventListener('click', (ev) => {
+    if (blockCard) return;
     x.classList.add('flip');
     if (!hasFlipCard) {
       hasFlipCard = true;
@@ -21,8 +25,9 @@ cards.forEach((x) => {
 
 // Card matching check
 function checkCards() {
+
   if (oneCard.dataset.comic === twoCard.dataset.comic) {
-    openClass();
+    setTimeout(() => openClass(), 500);
     return;
   }
   closeCards();
@@ -36,8 +41,21 @@ function openClass() {
 
 //  Close cards
 function closeCards() {
+  blockCard = true;
   setTimeout(() => {
     oneCard.classList.remove('flip');
     twoCard.classList.remove('flip');
+    blockCard = false;
   }, 500);
 }
+
+function randomCard() {
+  cards.forEach(x => {
+    let randomNumb = Math.floor(Math.random() * cardlist.length);
+    x.style.order =randomNumb
+  })
+}
+
+
+
+randomCard()
